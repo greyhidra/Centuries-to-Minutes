54145102bc1c3c1652975752b66baf54e03c156c
greyhidra <157640414+greyhidra@users.noreply.github.com>
2024-09-17 12:30:49 -0700

    Add files via upload
    test

---
+++ Centuries to Minutes C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centuries_to_Minutes_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vekovi = int.Parse(Console.ReadLine());
+
+            int godine = vekovi * 100;
+            int dani = vekovi * 36524 + (vekovi / 4);
+            long sati = dani * 24;
+            long minuti = sati * 60;
+
+
+            Console.WriteLine($"{vekovi} centuries = {godine} years = {dani} days = {sati} hours = {minuti} minutes");
+        }
+    }
+}
